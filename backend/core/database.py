da1ade75dcd4ca9484aa5006d921b8919b72e675
timsidorin <timsidorin@gmail.com>
2025-02-26 19:18:58 +0700

    move database.py /core

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, declared_attr
-from core.config import get_db_url
+from config import get_db_url
 
 
 
