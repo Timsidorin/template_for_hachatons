62e02fe55997bdd5a4b4c2c4a0cb2c7894eec326
timsidorin <timsidorin@gmail.com>
2025-03-01 20:13:47 +0700

    Первоначальная работа с БД +

--- backend/models/books.py
+++ backend/models/books.py
@@ -17,10 +17,31 @@ class Book(Base):
     date_publishing: Mapped[datetime] = mapped_column(sa.DateTime)
 
 
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+# async def create_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
 
-if __name__ == '__main__':
+
+async def create_book_example():
+    # Получаем сессию через генератор
+    session_gen = get_async_session()
+
+
+        # Получаем сессию из асинхронного генератора
+    session = await session_gen.__anext__()
+        # Создаем книгу
+    new_book = Book(
+            title="Преступление и наказание",
+            annotation="Роман о нравственных терзаниях",
+            date_publishing=datetime(1866, 1, 1),
+        )
+
+        # Добавляем и коммитим
+    session.add(new_book)
+    await session.commit()
+    print("Книга успешно добавлена!")
+
+# Запуск
+if __name__ == "__main__":
     import asyncio
-    asyncio.run(create_tables())
+    asyncio.run(create_book_example())
